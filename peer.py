5eec5f556d7271f4fee452426b97afd6b3640052
Venla Katainen <venla.katainen@gmail.com>
2024-02-26 12:28:22 +0200

    add error handling and edit output prints

--- peer.py
+++ peer.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 import sys
+from datetime import datetime
+
 
 
 # function to receive data
@@ -8,27 +10,37 @@ def receive_data(s):
     #while True:
     # receive message
     received_msg = s.recv(1024).decode()
-    # if message is quite, close the connection
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    socket_name = s.getsockname()[0]
+    
+    # if message is quit, close the connection
     if (received_msg.lower() == "quit"):
-        print(s.getsockname()[0], "left from the chat.")
+        print(f"{time} {socket_name} left from the chat.")
         s.close()
         
     # print message and address who send the message
     #if received_msg == "":
-        
-    print(s.getsockname()[0], ": ", received_msg)
-    s.close()
+    else:
+        print(f"{time} {socket_name} : {received_msg}")    
+        #print(s.getsockname()[0], ": ", received_msg)
+        s.close()
 
-# function to send (private) messages
-def send_message(s):
+# function to send messages
+def send_message(s, sender_ip):
     #while True:
     # encode message taken from the input
     message = input("Enter message: ")
     msg_to_send = message.encode('utf-8')
+
+    # show sent message to sender terminal as well
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{time} {sender_ip} : {message}")    
+
     for i in range(len(s)):
         # send message
         s[i].sendall(msg_to_send)
         if message.lower() == "quit":
+            print(f"{time} {sender_ip} left from the chat.")
             s[i].close()
             
         s[i].close()
@@ -37,7 +49,7 @@ def send_message(s):
 def handle_connections(s):
     while True:
         coming_socket, coming_address = s.accept()
-        print("Connection from: ", coming_address)
+        print(f"Connection from: {coming_address}")
         
         # Start a thread to handle the incoming connection
         receive_thread = threading.Thread(target=receive_data, args=([coming_socket]))
@@ -55,6 +67,7 @@ def handle_server_connection(s):
 
 
 # function to send group messages
+"""
 def send_group_message(group_members):    
     
     group_msg = input("Enter the message")
@@ -70,7 +83,7 @@ def send_group_message(group_members):
             member_socket.close()
             break
         
-
+"""
     
 
 
@@ -125,7 +138,7 @@ if __name__ == '__main__':
 
     """
     #if select == "2":
-    print("Private message selected. ")
+    #print("Private message selected. ")
     # handle private message transmission
     # Connect to another peer
     peer_address = "127.0.0.1" #input("Enter IP address of the friend: ")
@@ -138,15 +151,23 @@ if __name__ == '__main__':
         print("Connection establishment started...")
         sockets = []
         for i in range(len(ports)):
-            peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            peer_socket.connect((peer_address, ports[i]))
+            try:
+                peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                peer_socket.connect((peer_address, ports[i]))
+            except ConnectionRefusedError:
+                print(f" Port {ports[i]} not available.")
+                continue
+
             sockets.append(peer_socket)
         
-        print("Connected to: ", peer_address)
+        print("Connected to: ", sockets)
 
         # create thread to send messages
+        if len(sockets) > 0:
+            send_message(sockets, own_ip)
         
-        send_message(sockets)
+        else:
+            print("No available sockets added.")
         #send_message_thread = threading.Thread(target = send_message, args = ([peer_socket]))
         # start thread
         #send_message_thread.start()
