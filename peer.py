dd9b0dbeb2f46d60ff0460eb5b68951d23add168
Venla Katainen <venla.katainen@gmail.com>
2024-03-02 19:17:35 +0200

    server send group info for members does not work

--- peer.py
+++ peer.py
@@ -7,8 +7,8 @@ import pickle
 
 # function to receive data
 def receive_data(s):
-    #while True:
     # receive message
+
     received_msg = s.recv(1024).decode()
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     socket_name = s.getsockname()[0]
@@ -18,87 +18,64 @@ def receive_data(s):
         print(f"{time} {socket_name} left from the chat.")
         s.close()
         
-    # print message and address who send the message
-    #if received_msg == "":
     else:
-        print(f"{time} {socket_name} : {received_msg}")    
-        #print(s.getsockname()[0], ": ", received_msg)
+        print(f"{time} {socket_name} : {received_msg}")
         s.close()
+    
 
 
 
 # function to send messages
-def send_message(s, sender_ip):
-    #while True:
+def send_message(s, sender_ip, message):
+    
     # encode message taken from the input
-    message = input("Enter message: ")
     msg_to_send = message.encode('utf-8')
 
     # show sent message to sender terminal as well
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f"{time} {sender_ip} : {message}")    
-
-    for i in range(len(s)):
-        # send message
-        s[i].sendall(msg_to_send)
-        if message.lower() == "quit":
-            print(f"{time} {sender_ip} left from the chat.")
-            s[i].close()
-            
-        s[i].close()
-
+    s.sendall(msg_to_send)
+    
 
 
 # function to handle coming connections
 def handle_connections(s):
     
     while True:
+        # coming connection
         coming_socket, coming_address = s.accept()
         print(f"Connection from: {coming_address}")
-        
+    
         # Start a thread to handle the incoming connection
         receive_thread = threading.Thread(target=receive_data, args=([coming_socket]))
         receive_thread.start()
 
 
+
+
 # function to handle connection to the server
-def handle_server_connection(s):
-    #server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', 11111))
-    #while True:
+        
+def handle_server_connection():
+
+    # create socket for server connection
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind socket to certain ip + port
+    server_socket.bind((own_ip, own_port+2))
+    # connect to the server
+    server_socket.connect(('127.0.0.1', 11111))
+    # enter join / leave message
     message = input("Enter message: ")
+    # encode message
     msg_to_send = message.encode('utf-8')
-
-    s.send(msg_to_send)
-        
-        #listen_server_thread = threading.Thread(target=handle_connections, args=([s]))
-        #listen_server_thread.start()
-            #server_socket.send("I am CLIENT\n".encode())
-    from_server = pickle.loads(s.recv(1024))
-    print(from_server)
-
-
-
-
-# function to send group messages
-"""
-def send_group_message(group_members):    
+    # send message to the server
+    server_socket.send(msg_to_send)
+    # receive group info from the server
+    from_server = pickle.loads(server_socket.recv(1024))
+    # print group info
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{time} : {from_server}") 
     
-    group_msg = input("Enter the message")
 
-    for i in range(len(group_members)):
-        member_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        member_socket.connect(group_members[i])
-
-        msg_to_send = group_msg.encode('utf-8')
-        # send message
-        member_socket.sendall(msg_to_send)
-        if group_msg.lower() == "quit":
-            member_socket.close()
-            break
-        
-"""
-    
 
 
 if __name__ == '__main__':
@@ -115,80 +92,52 @@ if __name__ == '__main__':
     # set IP address and port to the socket
     me.bind((own_ip, own_port))
     # listen connections from other peers
-    #me.listen(5)
+    me.listen(5)
 
     print("Peer listening on {}:{}\n\n".format(own_ip, own_port))
 
     # start thread to handle incoming connections
-    #connection_handle_thread = threading.Thread(target=handle_connections, args=([me]))
-    #connection_handle_thread.start()
+    connection_handle_thread = threading.Thread(target=handle_connections, args=([me]))
+    connection_handle_thread.start()
 
-    # start thread to handle connection to the server
-    
-    server_thread = threading.Thread(target=handle_server_connection, args=([me]))
-    server_thread.start()
-    
-
-    # selection to group or private message
-    #select = input("If you want to send group message - write 1 \n If you want to send private message - write 2")
-    """
-    if select == "1":
-        friends = []
-        print("Group message selected.")
-        while True:
-            print("Enter all the IPs of the friends and ports one by one")
-            print("Write D when all the friends are added")
-            group_peer_address = input("Enter IP address of the friend: ")
-            
-            if group_peer_address.upper() == "D":
-                break
-            
-            group_peer_port = int(input("Enter port of the friend: "))
-
-            # add IP and port to the friends
-            friends.append((group_peer_address, group_peer_port))
-
-        print("Group formed.")
-        # send group message
-        send_group_message(friends)
-
-    """
-    #if select == "2":
-    #print("Private message selected. ")
-    # handle private message transmission
-    # Connect to another peer
-    
-    ######################## WORKING PART BELOW ######################################################
-    """
-    peer_address = "127.0.0.1" #input("Enter IP address of the friend: ")
-    
     while True:
-        #peer_port = int(input("Enter port of the friend: "))
-        num_of_ports = int(input("Enter number of ports: "))
-        ports = list(map(int, input("\nEnter the ports : ").strip().split()))[:num_of_ports]
-        # Connect to the other peer
-        print("Connection establishment started...")
-        sockets = []
-        for i in range(len(ports)):
-            try:
-                peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                peer_socket.connect((peer_address, ports[i]))
-            except ConnectionRefusedError:
-                print(f" Port {ports[i]} not available.")
-                continue
-
-            sockets.append(peer_socket)
         
-        print("Connected to: ", sockets)
-
-        # create thread to send messages
-        if len(sockets) > 0:
-            send_message(sockets, own_ip)
+        select = input("Join/leave group, select 1 \n send message, select 2 \n -> ")
         
-        else:
-            print("No available sockets added, message cannot be sent.")
-        #send_message_thread = threading.Thread(target = send_message, args = ([peer_socket]))
-        # start thread
-        #send_message_thread.start()
+        # handle group joining / leaving
+        
+        if (select == "1"):
+            print("selected 1")
+            handle_server_connection()
+        
+        
+        # handle message sending to other peers
 
-    """
+        if (select == "2"):
+            
+            peer_address = "127.0.0.1" #input("Enter IP address of the friend: ")
+  
+            #peer_port = int(input("Enter port of the friend: "))
+            num_of_ports = int(input("Enter number of ports: "))
+            ports = list(map(int, input("\nEnter the ports : ").strip().split()))[:num_of_ports]
+            message = input("Enter message: ")
+            # Connect to the other peer
+            print("Connection establishment started...")
+            
+            for i in range(len(ports)):
+                
+                try:
+                    peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    peer_socket.bind((own_ip, own_port+1))
+                    peer_socket.connect((peer_address, ports[i]))
+                    send_message(peer_socket,own_ip,message)
+                    
+                
+                except ConnectionRefusedError:
+                    print(f" Port {ports[i]} not available.")
+                    continue
+
+                
+
+
+        
--- server.py
+++ server.py
@@ -4,39 +4,36 @@ import json
 from datetime import datetime
 import pickle
 
+groups = {}
 
 
 # function receive data from the client socket
 def receive_data(conn, address):
-    while True:
-        # receive message
-        received_msg = conn.recv(1024).decode()
-        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        
-        if received_msg == "":
-            continue
 
+    # receive message
+    received_msg = conn.recv(1024).decode()
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    if received_msg != "":
         # print message and address who send the message
         print(f"{time} {address[0]} : {received_msg}")
 
-        # if message contains /join, the peer wants to join group
-        if "/join" in received_msg:
-            received_msg.replace(" ", "")
-            group_to_join = received_msg.replace("/join","")
+
+    # if message contains /join, the peer wants to join group
+    if "/join" in received_msg:
+        received_msg.replace(" ", "")
+        group_to_join = received_msg.replace("/join","")
+        # add peer to the group
+        group_info = join_group(group_to_join, address[0], str(int(address[1])-2))
+        print(group_info)
+        data=pickle.dumps(group_info)
+        # send group members back to the peer
+        conn.send(data)
+        update_group_members(group_to_join, group_info)
             
-            group_info = join_group(group_to_join, address[0], address[1])
-            print(group_info)
-            data=pickle.dumps(group_info)
-            # send group members back to the peer
-            conn.send(data)
-            
-    
-    
-        
-           
     
 
-# function to handle joining groups   
+# function to handle joining groups -> add peer to the group  
 def join_group (group_name, socket_ip, socket_port):
 
     # read group information from file
@@ -62,94 +59,68 @@ def join_group (group_name, socket_ip, socket_port):
     else:
         groups[group_name] = [(socket_ip, socket_port)]
 
-    temp_groups = groups
-
     # update group information to the file
     with open('groups_server.txt', 'w') as convert_file: 
         convert_file.write(json.dumps(groups))
 
     # send group information for the peer that joined and other group members.
-    group_members = temp_groups[group_name]
+    group_members = groups[group_name]
     # return group members of the group that peer wanted to join
     return group_members 
     
+
+# create socket for all group members and send updated group information to the group members
+def update_group_members (name_of_the_group, mem_of_group):
     
-def update_group_members (groups, group_name):
-    peers_list = groups[group_name]
-    # list for sockets
-    sockets = []
     # create sockets for all group members
-    for i in range(len(peers_list)):
-        peer_address = peers_list[i][0]
-        peer_port = peers_list[i][1]
+    for i in range(len(mem_of_group)):
+        peer_address = mem_of_group[i][0]
+        peer_port = int(mem_of_group[i][1])
+        print(peer_port)
         try:
             peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            peer_socket.connect((peer_address, peer_port))
+            peer_socket.bind((server_ip, server_port+1))
+            peer_socket.connect((peer_address,peer_port))
+            send_message(name_of_the_group, mem_of_group, peer_socket)
+            
         except ConnectionRefusedError:
-            print(f" Port {peer_port} not available.")
+            print(f"{mem_of_group[i]} not available.")
             continue
 
-        sockets.append(peer_socket)
-    
-    if len(sockets) > 0:
-        send_message_to_group(group_name, peers_list, sockets)
+        
+    print("group info sent")
+
 
 
 # send message to group 
-def send_message_to_group (group_name, group_members, group_member_sockets):
+def send_message(group_name, group_members, group_member_socket):
     
     # add group name to the first of the member list
-    group_members = group_members.insert(0,group_name)
-    # pickle to send list 
-    data=pickle.dumps(group_members)
-    
-    # send data to sockets 
-    for i in range(len(group_member_sockets)):
-        member_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        member_socket.connect(group_member_sockets[i])
+    group_members_group_name = (group_name, group_members)
 
-        # send message
-        member_socket.sendall(data)
+    # send data to sockets 
+    group_member_socket.send(str(group_members_group_name).encode())
+   
        
         
-
-
-# function to send data 
-def send_message(conn):
-    while True:
-        # take input and enocde
-        message_to_send = input().encode()
-        # send message
-        conn.sendall(message_to_send)
-
-
-
-# function to handle coming connections
-def handle_connections(socket_connection):
-    while True:
-        coming_socket, coming_address = socket_connection.accept()
-        print("Connection from: ", coming_address)
-        
-        # Start a thread to handle the incoming connection
-        receive_thread = threading.Thread(target=receive_data, args=([coming_socket, coming_address]))
-        receive_thread.start()
-        
-        #send_thread = threading.Thread(target = send_message, args = ([coming_socket]))
-        #send_thread.start()
-
-     
-
-
 
 if __name__ == '__main__':
     # create socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # set IP address and port to the socket
-    s.bind(('127.0.0.1', 11111))
+    server_ip = '127.0.0.1'
+    server_port = 11111
+    s.bind((server_ip, server_port))
     # listen connections from clients
-    s.listen(4)
+    s.listen(5)
 
-    # handle coming client connections
-    coming_connection = threading.Thread(target=handle_connections, args=([s]))
-    coming_connection.start()
+    while True:
+        coming_socket, coming_address = s.accept()
+        print("Connection from: ", coming_address)
+
+        # handle coming client connections
+        coming_connection = threading.Thread(target=receive_data, args=([coming_socket, coming_address]))
+        coming_connection.start()
+
+    
     
